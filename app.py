6f0147c75110292ab21cfa0fd13514d6e474b9d1
Kusalya Kaluthotage <kusalyakaluthotage@gmail.com>
2025-04-23 20:04:46 +0530

    raw data printing

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ st.set_page_config(page_title="COVID-19 Dashboard - Sri Lanka", layout="wide")
 
 def load_data():
     df = pd.read_csv("monthly data.csv")
-    df['Date'] = pd.to_datetime(df['Data'], format='%bb-%y')
+    df.columns = df.columns.str.strip()
     return df
+
+df = load_data()
+
+
+st.title ("COVID-19 Dashboard - Sri Lanka")
+st.markdown("This dashboard provides insights on COVID-19 cases across different districs in Sri Lanka.")
+
+with st.expander("View raw data"):
+    st.dataframe(df)
+
 
