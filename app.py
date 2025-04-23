aea8a3ca8dacd09ab5bc5ab42cebd050047ca12f
Kusalya Kaluthotage <kusalyakaluthotage@gmail.com>
2025-04-23 19:40:07 +0530

    load dataser

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(page_title="COVID-19 Dashboard - Sri Lanka", layout="wide")
+
+def load_data():
+    df = pd.read_csv("monthly data.csv")
+    df['Date'] = pd.to_datetime(df['Data'], format='%bb-%y')
+    return df
+
